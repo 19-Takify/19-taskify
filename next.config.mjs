d233ed3a87ca4629629856df78a598d12995e0d9
devwqc <devwqc@gmail.com>
2024-04-15 12:24:05 +0900

    chore: 프로젝트 폴더 구조 생성

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
-import path from "path";
+import path from 'path';
 
 // path.join(__dirname, "styles") 코드와 동일
 const includePaths = [
-  path.join(path.dirname(new URL(import.meta.url).pathname), "styles"),
+  path.join(path.dirname(new URL(import.meta.url).pathname), 'styles'),
 ];
 
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   sassOptions: {
     includePaths: [includePaths],
     prependData: `@import "src/styles/colors.scss";`,
