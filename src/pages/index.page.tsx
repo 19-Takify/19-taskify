e87b16a5258b85ff52a783d9589d1e15c0affda6
skoo1100 <skoo062125@gmail.com>
2024-04-16 01:20:13 +0900

    refactor: 버튼 컴포넌트 수정

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,11 +1,8 @@
-import Button from '@/components/Button/Button';
-import ButtonDashboard from '@/components/Button/ButtonDashboard';
 import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
-      <Button usage="signin" />
       <Head>Taskify</Head>
       <h1>taskify</h1>
     </>
