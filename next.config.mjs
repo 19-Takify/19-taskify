9b0f6ca59efb4969142c0f7be1495793d531f6b1
devwqc <devwqc@gmail.com>
2024-04-15 13:15:04 +0900

    refactor: _app.page.tsx에 reset.scss 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 import path from 'path';
 
-// path.join(__dirname, "styles") 코드와 동일
 const includePaths = path.join(
   path.dirname(new URL(import.meta.url).pathname),
   'styles',
@@ -11,7 +10,7 @@ const nextConfig = {
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   sassOptions: {
     includePaths: [includePaths],
-    prependData: `@import "src/styles/global.scss";`,
+    prependData: `@import "src/styles/colors.scss";`,
   },
 };
 
