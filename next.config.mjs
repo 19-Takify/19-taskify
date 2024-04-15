a91fc12bfcd5f51697a1418bdb4f9f2cf5812d11
woo29 <dldngur37@naver.com>
2024-04-15 11:45:21 +0900

    chore: next.config scss 전역 import 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+import path from "path";
+
+// path.join(__dirname, "styles") 코드와 동일
+const includePaths = [
+  path.join(path.dirname(new URL(import.meta.url).pathname), "styles"),
+];
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [includePaths],
+    prependData: `@import "src/styles/colors.scss";`,
+  },
 };
 
 export default nextConfig;
