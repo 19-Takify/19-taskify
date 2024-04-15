8162daf5e995ffa723c05561564af445cfd3036f
skoo1100 <skoo062125@gmail.com>
2024-04-16 00:06:27 +0900

    fix: 버튼 컴포넌트 수정 및 템플릿 수정

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,4 +1,5 @@
-import Button from '@/components/Button';
+import Button from '@/components/Button/Button';
+import ButtonDashboard from '@/components/Button/ButtonDashboard';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
