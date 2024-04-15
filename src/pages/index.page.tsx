ee02cb5687ab3fb7db5d2d27715face1b4561c0c
skoo1100 <skoo062125@gmail.com>
2024-04-16 02:17:27 +0900

    feat: card 컴포넌트 추가

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,8 +1,10 @@
 import Head from 'next/head';
+import Card from '@/components/Card';
 
 export default function Home() {
   return (
     <>
+      <Card />
       <Head>Taskify</Head>
       <h1>taskify</h1>
     </>
