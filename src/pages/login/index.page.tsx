e7052d43332134fe976a49ee56158decccbf57a8
Songeun <hannahkeeien@gmail.com>
2024-04-16 19:06:25 +0900

    feat: Input 컴포넌트 작업

--- src/pages/login/index.page.tsx
+++ src/pages/login/index.page.tsx
@@ -1,5 +1,83 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SubmitErrorHandler, SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
+import Input from '@/components/Input';
+
+interface IFormValues {
+  email: string;
+  password: string;
+  passwordConfirm: string;
+}
+
+const schema = z.object({
+  email: z
+    .string()
+    .min(1, { message: '이메일을 입력해 주세요.' })
+    .email({ message: '이메일 형식으로 작성해 주세요.' }),
+  password: z
+    .string()
+    .min(1, { message: '비밀번호를 입력해 주세요.' })
+    .min(8, { message: '8자 이상 입력해주세요.' }),
+  passwordConfirm: z.string().min(1),
+});
+
 function Login() {
-  return <div>Login</div>;
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IFormValues>({
+    resolver: zodResolver(schema),
+    mode: 'onChange',
+    defaultValues: {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    },
+  });
+  const onValid: SubmitHandler<IFormValues> = (data) => {
+    console.log(data);
+  };
+  const onInvalid: SubmitErrorHandler<IFormValues> = (data) => {
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
+      <div>
+        <div>
+          <Input
+            label="이메일"
+            id="email"
+            register={register}
+            required
+            placeholder="이메일을 입력해주세요."
+            errors={errors}
+          />
+        </div>
+        <div>
+          <Input
+            id="password"
+            icon="calendar"
+            register={register}
+            required
+            placeholder="비밀번호를 입력해주세요."
+          />
+          <p>{errors.password?.message}</p>
+        </div>
+        <div>
+          <Input
+            id="passwordConfirm"
+            type="password"
+            register={register}
+            required
+            placeholder="비밀번호를 입력해 주세요."
+          />
+          <p>{errors.passwordConfirm?.message}</p>
+        </div>
+      </div>
+      <button type="submit">submit</button>
+    </form>
+  );
 }
 
 export default Login;
