f46eeceeaa79226b9a7df2c01da8bc9d956f9762
woo29 <dldngur37@naver.com>
2024-04-16 16:21:36 +0900

    refactor: 피드백 반영

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,15 +1,3 @@
-import setToast from '@/utils/setToast';
-import { ToastText } from '@/constants/toastText';
-
 export default function Home() {
-  return (
-    <>
-      <button onClick={() => setToast(ToastText.success, 'ㅇ_ㅇ!')}>
-        success
-      </button>
-      <button onClick={() => setToast(ToastText.error, 'ㅇ_ㅇ!')}>error</button>
-      <button onClick={() => setToast(ToastText.info, 'ㅇ_ㅇ!')}>info</button>
-      <button onClick={() => setToast(ToastText.warn, 'ㅇ_ㅇ!')}>warn</button>
-    </>
-  );
+  return <></>;
 }
