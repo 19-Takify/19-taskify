f82b3b9968c8b00be842718fe3b0c62f83861691
yubin <yb3143@gmail.com>
2024-04-16 20:24:46 +0900

    [#9] feat: header 생성

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import styles from './Header.module.scss';
+import MenuIcon from '/public/svgs/menu.svg';
+import SettngIcon from '/public/svgs/setting.svg';
+import InviteIcon from '/public/svgs/invite.svg';
+
+function Header() {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.box}>
+          <Image className={styles.menu} src={MenuIcon} alt="menu" />
+          <div className={styles.text}>내 대시보드</div>
+        </div>
+        <div className={styles.box}>
+          <button className={styles.button}>
+            <Image className={styles.icon} src={SettngIcon} alt="setting" />
+            관리
+          </button>
+          <button className={styles.button}>
+            <Image className={styles.icon} src={InviteIcon} alt="invite" />
+            초대하기
+          </button>
+          <div className={styles.box}>
+            <div className={styles.img}></div>
+            <div className={styles.name}>박유빈</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
