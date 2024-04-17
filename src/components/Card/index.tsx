fe630a65153761693e50ed7fa9b5d1076a9c9e20
skoo1100 <skoo062125@gmail.com>
2024-04-17 14:12:11 +0900

    test: card 데이터 불러오기

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,42 +1,44 @@
 import styles from './Card.module.scss';
 import Image from 'next/image';
 
-function Card() {
-  const card = [
-    {
-      id: 0,
-      title: '새로운 작업물',
-      description: 'string',
-      tags: ['프론트엔드', '백엔드'],
-      dueDate: '2024.6.21',
-      assignee: {
-        profileImageUrl: '/svgs/crown.svg',
-        nickname: '홍길동',
-        id: 0,
-      },
-      imageUrl: '',
-      teamId: 'string',
-      columnId: 0,
-      createdAt: '2024-04-15T16:25:50.458Z',
-      updatedAt: '2024-04-15T16:25:50.458Z',
-    },
-  ];
+type Assignee = {
+  profileImageUrl: string;
+  nickname: string;
+  id: number;
+};
 
+type CardList = {
+  id: number;
+  title: string;
+  description?: string;
+  tags?: string[];
+  dueDate?: string;
+  assignee?: Assignee;
+  imageUrl?: string;
+  teamId?: string;
+  columnId: number;
+  createdAt?: string;
+  updatedAt?: string;
+};
+
+type CardProps = {
+  cards: CardList;
+};
+
+function Card({ cards }: CardProps) {
   return (
     <div className={styles.cardBox}>
-      {card[0].imageUrl && (
+      {cards.imageUrl && (
         <Image
-          src={card[0].imageUrl}
+          src={cards.imageUrl}
           alt=" card 배경 이미지"
           width={274}
           height={160}
         />
       )}
-      <span>{card[0].title}</span>
+      <span>{cards.title}</span>
       <ul className={styles.tags}>
-        {card[0].tags.map((tag, index) => (
-          <li key={index}>{tag}</li>
-        ))}
+        {cards.tags?.map((tag, index) => <li key={index}>{tag}</li>)}
       </ul>
       <div className={styles.bottom}>
         <div className={styles.date}>
@@ -46,11 +48,12 @@ function Card() {
             width={18}
             height={18}
           />
-          <p>{card[0].dueDate}</p>
+          <p>{cards.dueDate}</p>
         </div>
         <Image
-          src={card[0].assignee.profileImageUrl}
-          alt={`${card[0].assignee.nickname} 프로필 이미지`}
+          className={styles.profileIcon}
+          src={cards.assignee?.profileImageUrl || ''}
+          alt={`${cards.assignee?.nickname} 프로필 이미지`}
           width={24}
           height={24}
         />
