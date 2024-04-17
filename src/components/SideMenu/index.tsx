f055dc8962324302be31354fa3f238edf009e7c9
skoo1100 <skoo062125@gmail.com>
2024-04-17 13:28:49 +0900

    feat: sidemenu 애니메이션 추가

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,6 +1,7 @@
 import styles from './SideMenu.module.scss';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 type DashboardList<T extends string | boolean | number> = {
   [key: string]: T;
@@ -13,53 +14,68 @@ type SideMenuProps = {
 
 function SideMenu({ dashboards, isOpen }: SideMenuProps) {
   const router = useRouter();
+  const [renderDelayed, setRenderDelayed] = useState(false);
 
   const handleDashboardClick = (id: number) => {
+    //페이지 이동
     router.push(`/dashboard/${id}`);
   };
 
+  useEffect(() => {
+    //렌더링시 0.4초 뒤에 작동 >> 초기 애니메이션 제거
+    const timeout = setTimeout(() => {
+      setRenderDelayed(true);
+    }, 400);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div className={`${styles.sideMenu} ${isOpen && styles.open}`}>
-      <Image
-        src="/svgs/taskify-logo.svg"
-        alt="taskify 로고 이미지"
-        width={109}
-        height={34}
-      />
-      <div className={styles.dashboardMenu}>
-        <div className={styles.dashboardAddBox}>
-          <span>Dash Boards</span>
+      {renderDelayed && (
+        <div className={styles.sideMenuBox}>
           <Image
-            src="/svgs/add-box.svg"
-            alt="대시보드 추가 이미지"
-            width={20}
-            height={20}
+            src="/svgs/taskify-logo.svg"
+            alt="taskify 로고 이미지"
+            width={109}
+            height={34}
           />
-        </div>
-        <ul className={styles.dashboardListBox}>
-          {dashboards.map((dashboard) => (
-            <li
-              key={dashboard.id as number}
-              className={styles.dashboardList}
-              onClick={() => handleDashboardClick(dashboard.id as number)}
-            >
-              <div
-                className={styles.circle}
-                style={{ backgroundColor: `${dashboard.color}` }}
+          <div className={styles.dashboardMenu}>
+            <div className={styles.dashboardAddBox}>
+              <span>Dash Boards</span>
+              <Image
+                src="/svgs/add-box.svg"
+                alt="대시보드 추가 이미지"
+                width={20}
+                height={20}
               />
-              <p>{dashboard.title}</p>
-              {dashboard.createdByMe && (
-                <Image
-                  src="/svgs/crown.svg"
-                  alt="crown 이미지"
-                  width={20}
-                  height={16}
-                />
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
+            </div>
+            <ul className={styles.dashboardListBox}>
+              {dashboards.map((dashboard) => (
+                <li
+                  key={dashboard.id as number}
+                  className={styles.dashboardList}
+                  onClick={() => handleDashboardClick(dashboard.id as number)}
+                >
+                  <div
+                    className={styles.circle}
+                    style={{ backgroundColor: `${dashboard.color}` }}
+                  />
+                  <p>{dashboard.title}</p>
+                  {dashboard.createdByMe && (
+                    <Image
+                      src="/svgs/crown.svg"
+                      alt="crown 이미지"
+                      width={20}
+                      height={16}
+                    />
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,31 +1,8 @@
-import SideMenu from '@/components/SideMenu';
 import Head from 'next/head';
 
 export default function Home() {
-  const dashboards = [
-    {
-      id: 1,
-      title: '홍길동',
-      color: '#7853ca',
-      createdByMe: true,
-    },
-    {
-      id: 2,
-      title: '홍길순',
-      color: '#ec83c4',
-      createdByMe: true,
-    },
-    {
-      id: 3,
-      title: '홍길창',
-      color: '#3873ca',
-      createdByMe: true,
-    },
-  ];
-
   return (
     <div>
-      <SideMenu dashboards={dashboards} isOpen={true} />
       <Head>Taskify</Head>
       <h1>taskify</h1>
     </div>
