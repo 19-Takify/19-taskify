ec90dc6673a76e817cf55ac9dfe0243de5f671fe
Songeun <hannahkeeien@gmail.com>
2024-04-17 16:12:49 +0900

    feat: Input 컴포넌트 적용

--- src/pages/login/index.page.tsx
+++ src/pages/login/index.page.tsx
@@ -1,5 +1,10 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { SubmitErrorHandler, SubmitHandler, useForm } from 'react-hook-form';
+import {
+  Controller,
+  SubmitErrorHandler,
+  SubmitHandler,
+  useForm,
+} from 'react-hook-form';
 import { z } from 'zod';
 import Input from '@/components/Inputs/Input';
 import DatePicker from '@/components/Inputs/DatePicker';
