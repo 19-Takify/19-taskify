b8110d3a9354f046982856b6b3a83d3af2e98ba6
woo29 <dldngur37@naver.com>
2024-04-17 15:36:51 +0900

    feat: Dropdown 컴포넌트 생성

---
+++ src/components/Dropdown/Item/index.tsx
@@ -0,0 +1,40 @@
+import styles from './Item.module.scss';
+
+type TData = {
+  title?: string;
+  nickname?: string;
+  profileImageUrl?: string;
+};
+
+type TItemProps = {
+  usage?: 'state' | 'manager';
+  data: TData;
+};
+
+function Item({ usage, data }: TItemProps) {
+  const { title, nickname, profileImageUrl } = data;
+  const isState = usage === 'state';
+
+  return (
+    <div className={styles.wrap}>
+      <div className={`${styles.box} ${isState && styles.backEffect}`}>
+        {isState ? (
+          <>
+            <p className={styles.circle} />
+            <span className={styles.text}>{title}</span>
+          </>
+        ) : (
+          <>
+            <p
+              className={styles.profile}
+              style={{ backgroundImage: `url(${profileImageUrl})` }}
+            />
+            <span className={styles.nickname}>{nickname}</span>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Item;
