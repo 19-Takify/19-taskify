5ff11df5e79a79caa873d906c9ae6c724d764375
skoo1100 <skoo062125@gmail.com>
2024-04-18 21:18:52 +0900

    fix: 프로필 리스트 수정

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,20 +1,3 @@
-import ModalButton from '@/components/Button/ModalButton';
-import PageButton from '@/components/Button/PageButton';
-import ProfileIconList from '@/components/Profile/ProfileIconList';
-
 export default function Home() {
-  return (
-    <>
-      <ProfileIconList
-        profileList={[
-          { profileImageUrl: '', nickname: '길동', id: 2 },
-          { profileImageUrl: '', nickname: '길동', id: 2 },
-          { profileImageUrl: '', nickname: '길동', id: 2 },
-          { profileImageUrl: '', nickname: '길동', id: 2 },
-          { profileImageUrl: '', nickname: '길동', id: 2 },
-          { profileImageUrl: '', nickname: '길동', id: 2 },
-        ]}
-      ></ProfileIconList>
-    </>
-  );
+  return <></>;
 }
