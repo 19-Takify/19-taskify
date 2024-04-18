37c55e6db6a7ae197abd2ea104db209299a31756
skoo1100 <skoo062125@gmail.com>
2024-04-18 21:18:52 +0900

    fix: 프로필 리스트 수정

--- src/components/Profile/ProfileIconList/index.tsx
+++ src/components/Profile/ProfileIconList/index.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import useIsDesiredSize from '@/hooks/useIsDesiredSize';
 import ProfileIcon from '../ProfileIcon';
 import styles from './ProfileIconList.module.scss';
 
@@ -13,47 +14,22 @@ type ProfileIconListProps = {
 };
 
 function ProfileIconList({ profileList }: ProfileIconListProps) {
-  const [isCount, setIsCount] = useState(2);
-
-  console.log(isCount);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (typeof window !== 'undefined') {
-        if (window.innerWidth >= 375) {
-          setIsCount(4);
-          return;
-        }
-        setIsCount(2);
-      }
-    };
-
-    // 컴포넌트가 마운트될 때 한 번 실행
-    handleResize();
-
-    // 창 크기가 변경될 때마다 실행
-    window.addEventListener('resize', handleResize);
-
-    // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, [setIsCount]); // setIsMobile가 의존성 배열에 추가됨
+  const isTablet = useIsDesiredSize(744);
+  const profileCount = isTablet ? 2 : 4;
+  const profileListSlice = profileList.slice(0, profileCount + 1);
 
   return (
     <ul className={styles.profileIconList}>
-      {profileList.map((profile, index) =>
-        index <= isCount ? (
-          index === isCount ? (
-            <li key={profile.id}>
-              <ProfileIcon>{`+${profileList.length - isCount}`}</ProfileIcon>
-            </li>
-          ) : (
-            <li key={profile.id}>
-              <ProfileIcon profile={profile} />
-            </li>
-          )
-        ) : null,
+      {profileListSlice.map((profile, index) =>
+        index === profileCount ? (
+          <li key={profile.id}>
+            <ProfileIcon>{`+${profileList.length - profileCount}`}</ProfileIcon>
+          </li>
+        ) : (
+          <li key={profile.id}>
+            <ProfileIcon profile={profile} />
+          </li>
+        ),
       )}
     </ul>
   );
