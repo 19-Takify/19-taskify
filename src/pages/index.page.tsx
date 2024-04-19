0b12b4104b1d3cc181b31b38a1df4881809f7939
devwqc <devwqc@gmail.com>
2024-04-19 18:44:25 +0900

    test: 봉찬 비밀번호 변경, 내 정보 조회

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,3 +1,46 @@
+import axios from '@/apis/axios';
+
 export default function Home() {
-  return <></>;
+  const accessToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTc0OSwidGVhbUlkIjoiNC0xOSIsImlhdCI6MTcxMzUxOTU0OSwiaXNzIjoic3AtdGFza2lmeSJ9.yHOcu9ojaJrM3ox576Cr3Oj0xTW7g-3y2k2FHWMLUbk';
+  const handlePasswordChange = async () => {
+    try {
+      const res = await axios.put(
+        'auth/password',
+        {
+          password: '1234qwer',
+          newPassword: '12341234',
+        },
+        {
+          headers: {
+            Authorization: `bearer ${accessToken}`,
+          },
+        },
+      );
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleMe = async () => {
+    try {
+      const res = await axios.get('users/me', {
+        headers: {
+          Authorization: `bearer ${accessToken}`,
+        },
+      });
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <button type="button" onClick={handleMe}>
+        테스트
+      </button>
+    </>
+  );
 }
--- src/pages/login/index.page.tsx
+++ src/pages/login/index.page.tsx
@@ -42,8 +42,8 @@ function Login() {
     try {
       const response = await axios.post('auth/login', data);
       const result = response.data;
-      // console.log(result);
-      router.push('/mydashboard');
+      console.log(result);
+      // router.push('/mydashboard');
     } catch (error) {
       if (!isAxiosError(error)) {
         // `AxiosError`가 아닌 경우
