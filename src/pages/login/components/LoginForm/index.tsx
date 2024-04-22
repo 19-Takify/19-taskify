7dc35703eab8ec7328f322842ff5917027745668
Songeun <hannahkeeien@gmail.com>
2024-04-22 11:06:27 +0900

    design: LoginPage 스타일 적용

---
+++ src/pages/login/components/LoginForm/index.tsx
@@ -0,0 +1,107 @@
+import styles from './LoginForm.module.scss';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
+import Input from '@/components/Inputs/Input';
+import { isAxiosError } from 'axios';
+import axios from '@/apis/axios';
+import setToast from '@/utils/setToast';
+import { useRouter } from 'next/router';
+import PageButton from '@/components/Button/PageButton';
+import {
+  FETCH_ERROR_MESSAGE,
+  SERVER_ERROR_MESSAGE,
+  VALID_ERROR_MESSAGE,
+} from '@/constants/errorMessage';
+import { PAGE_PATH } from '@/constants/pageUrl';
+
+type FormValues = {
+  email: string;
+  password: string;
+};
+
+const schema = z.object({
+  email: z
+    .string()
+    .min(1, { message: VALID_ERROR_MESSAGE.EMAIL.EMPTY })
+    .email({ message: VALID_ERROR_MESSAGE.EMAIL.INVALID }),
+  password: z
+    .string()
+    .min(1, { message: VALID_ERROR_MESSAGE.PASSWORD.EMPTY })
+    .min(8, { message: VALID_ERROR_MESSAGE.MIN.EIGHT }),
+});
+
+function LoginForm() {
+  const router = useRouter();
+  const {
+    register,
+    formState: { errors, isValid, isSubmitting },
+    setError,
+    handleSubmit,
+  } = useForm<FormValues>({
+    resolver: zodResolver(schema),
+    mode: 'all',
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+  const handleValidSubmit: SubmitHandler<FormValues> = async (data) => {
+    try {
+      const response = await axios.post('auth/login', data);
+      const result = response.data;
+      router.push(PAGE_PATH.MY_DASHBOARD);
+    } catch (error) {
+      if (!isAxiosError(error)) {
+        // `AxiosError`가 아닌 경우
+        setToast('error', FETCH_ERROR_MESSAGE.UNKNOWN);
+        return;
+      }
+      // `AxiosError`인 경우 에러 처리
+      if (!error.response) {
+        setToast('error', FETCH_ERROR_MESSAGE.REQUEST);
+        return;
+      }
+      const message = error.response.data.message;
+      if (message === SERVER_ERROR_MESSAGE.USER.NOT_FOUND) {
+        setError('email', { message });
+        return;
+      }
+      setError('password', { message });
+    }
+  };
+
+  return (
+    <form
+      className={styles.container}
+      onSubmit={handleSubmit(handleValidSubmit)}
+    >
+      <div className={styles.inputContainer}>
+        <Input
+          label="이메일"
+          hasLabel
+          id="email"
+          type="email"
+          register={register('email')}
+          placeholder="이메일을 입력해 주세요."
+          errors={errors}
+        />
+
+        <Input
+          id="password"
+          label="비밀번호"
+          hasLabel
+          type="password"
+          register={register('password')}
+          placeholder="비밀번호를 입력해 주세요."
+          errors={errors}
+        />
+      </div>
+      <PageButton type="submit" disabled={!isValid || isSubmitting}>
+        로그인
+      </PageButton>
+    </form>
+  );
+}
+
+export default LoginForm;
--- src/pages/login/index.page.tsx
+++ src/pages/login/index.page.tsx
@@ -1,99 +1,34 @@
-import { zodResolver } from '@hookform/resolvers/zod';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { z } from 'zod';
-import Input from '@/components/Inputs/Input';
-import { isAxiosError } from 'axios';
-import axios from '@/apis/axios';
-import setToast from '@/utils/setToast';
-import { useRouter } from 'next/router';
-
-type FormValues = {
-  email: string;
-  password: string;
-};
-
-const schema = z.object({
-  email: z
-    .string()
-    .min(1, { message: '이메일을 입력해 주세요.' })
-    .email({ message: '이메일 형식으로 작성해 주세요.' }),
-  password: z
-    .string()
-    .min(1, { message: '비밀번호를 입력해 주세요.' })
-    .min(8, { message: '8자 이상 입력해주세요.' }),
-});
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './Login.module.scss';
+import { PAGE_PATH } from '@/constants/pageUrl';
+import LoginForm from './components/LoginForm';
 
 function Login() {
-  const router = useRouter();
-  const {
-    register,
-    formState: { errors, isValid },
-    setError,
-    handleSubmit,
-  } = useForm<FormValues>({
-    resolver: zodResolver(schema),
-    mode: 'all',
-    defaultValues: {
-      email: '',
-      password: '',
-    },
-  });
-  const handleValidSubmit: SubmitHandler<FormValues> = async (data) => {
-    try {
-      const response = await axios.post('auth/login', data);
-      const result = response.data;
-      // console.log(result);
-      router.push('/mydashboard');
-    } catch (error) {
-      if (!isAxiosError(error)) {
-        // `AxiosError`가 아닌 경우
-        setToast('error', '알 수 없는 오류가 발생했습니다.');
-        return;
-      }
-      // `AxiosError`인 경우 에러 처리
-      if (!error.response) {
-        setToast('error', '로그인에 실패했습니다.');
-        return;
-      }
-      const message = error.response.data.message;
-      if (message === '존재하지 않는 유저입니다.') {
-        setError('email', { message });
-        return;
-      }
-      setError('password', { message });
-    }
-  };
-
   return (
-    <form onSubmit={handleSubmit(handleValidSubmit)}>
-      <div>
-        <div>
-          <Input
-            label="이메일"
-            hasLabel
-            id="email"
-            type="email"
-            register={register('email')}
-            placeholder="이메일을 입력해 주세요."
-            errors={errors}
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <Link href={PAGE_PATH.MAIN}>
+          <Image
+            src="/svgs/taskify-vertical-logo.svg"
+            alt="Taskify 로고"
+            className={styles.logoImage}
+            width={200}
+            height={280}
           />
-        </div>
-        <div>
-          <Input
-            id="password"
-            label="비밀번호"
-            hasLabel
-            type="password"
-            register={register('password')}
-            placeholder="비밀번호를 입력해 주세요."
-            errors={errors}
-          />
-        </div>
-      </div>
-      <button disabled={!isValid} type="submit">
-        로그인
-      </button>
-    </form>
+        </Link>
+        <h2 className={styles.title}>오늘도 만나서 반가워요! </h2>
+      </header>
+      <LoginForm />
+      <footer>
+        <p className={styles.footerMessage}>
+          회원이 아니신가요?
+          <Link href={PAGE_PATH.SIGNUP} className={styles.link}>
+            회원가입하기
+          </Link>
+        </p>
+      </footer>
+    </div>
   );
 }
 
