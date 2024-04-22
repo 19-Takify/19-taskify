326f16672e1cc78f71f84310db2f494a7e50e1f3
woo29 <dldngur37@naver.com>
2024-04-22 10:46:46 +0900

    refactor: base url 환경 변수 사용

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
 const instance = axios.create({
-  baseURL: 'https://sp-taskify-api.vercel.app/4-19/',
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Mjk5MiwidGVhbUlkIjoiNC0xOSIsImlhdCI6MTcxMzYwNDc4OCwiaXNzIjoic3AtdGFza2lmeSJ9.C_9YGNBlt7urk1ZNlVqL6KCuFlWozrrAd6rz0Q0YM8M`,
--- src/pages/dashboard/edit/index.page.tsx
+++ src/pages/dashboard/edit/index.page.tsx
@@ -5,6 +5,7 @@ import setToast from '@/utils/setToast';
 import styles from './style/edit.page.module.scss';
 import DashboardManager from '@/pages/dashboard/edit/components/DashboardManager';
 import DashboardEdit from '@/pages/dashboard/edit/components/DashboardEdit';
+import Head from 'next/head';
 
 // 대시보드 삭제 버튼 - 대시보드 생성자(전역 상태 관리)한테만 보이게 조건부 렌더링, 컨펌 모달
 function Edit() {
@@ -24,6 +25,9 @@ function Edit() {
 
   return (
     <>
+      <Head>
+        <title>대시보드 수정</title>
+      </Head>
       <div style={{ border: '1px solid #000', height: '70px' }}>임시 헤더</div>
       <div className={styles.wrap}>
         <div className={styles.box}>
