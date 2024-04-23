7f8430734d854700af526db001d45d4e621fef21
devwqc <devwqc@gmail.com>
2024-04-23 15:08:31 +0900

    test: 봉찬 비밀번호 변경, 내 정보 조회

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,43 +1,8 @@
-import axios, { setContext } from '@/apis/axios';
-import { authAtom } from '@/stores/auth';
-import { useStore } from 'jotai';
-import { GetServerSidePropsContext } from 'next';
-
-// SSR에서 axios interceptor 값 받아와지지 않는 현상.
-// https://github.com/axios/axios/issues/4976#issuecomment-1723822982
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  // const cookies = context.req.cookies;
-  // const accessToken = cookies.accessToken;
-  // console.log('Home SSR accessToken: ', accessToken);
-  // if (accessToken) {
-  setContext(context);
-  try {
-    // const res = await axios.get('users/me', {
-    // headers: {
-    //   Authorization: `Bearer ${accessToken}`,
-    // },
-    // });
-    // console.log('me data: ', res.data);
-  } catch (error) {
-    console.log(error);
-  }
-  // }
-
-  return {
-    props: {},
-  };
-}
+import axios from '@/apis/axios';
 
 export default function Home() {
-  const store = useStore();
-
-  // console.log(store.set(authAtom, (pre) => ++pre));
-  // console.log(store.get(authAtom));
-  store.set(authAtom, 1);
-
   const accessToken =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTc0OSwidGVhbUlkIjoiNC0xOSIsImlhdCI6MTcxMzUyNTQ2NSwiaXNzIjoic3AtdGFza2lmeSJ9.tImTj9DbHCr42QnQaBwayyX_lflTnqdGrqWvu-LEvzkbong';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTc0OSwidGVhbUlkIjoiNC0xOSIsImlhdCI6MTcxMzUxOTU0OSwiaXNzIjoic3AtdGFza2lmeSJ9.yHOcu9ojaJrM3ox576Cr3Oj0xTW7g-3y2k2FHWMLUbk';
   const handlePasswordChange = async () => {
     try {
       const res = await axios.put(
@@ -48,7 +13,7 @@ export default function Home() {
         },
         {
           headers: {
-            Authorization: `Bearer ${accessToken}`,
+            Authorization: `bearer ${accessToken}`,
           },
         },
       );
@@ -61,9 +26,9 @@ export default function Home() {
   const handleMe = async () => {
     try {
       const res = await axios.get('users/me', {
-        // headers: {
-        //   Authorization: `Bearer ${accessToken}`,
-        // },
+        headers: {
+          Authorization: `bearer ${accessToken}`,
+        },
       });
       console.log(res);
     } catch (error) {
@@ -75,9 +40,6 @@ export default function Home() {
     <>
       <button type="button" onClick={handleMe}>
         테스트
-      </button>
-      <button type="button" onClick={() => store.set(authAtom, 2)}>
-        {store.get(authAtom)}
       </button>
     </>
   );
