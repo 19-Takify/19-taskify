eff601de91e0a93777187860d5580870cbd4bb2e
woo29 <dldngur37@naver.com>
2024-04-23 18:02:40 +0900

    feat: 헤더 관리 버튼 조건부 렌더링 추가

--- src/components/Header/DashBoard/index.tsx
+++ src/components/Header/DashBoard/index.tsx
@@ -15,6 +15,7 @@ function DashBoardHeader() {
   const isBreakPoint = useIsDesiredSize(550);
   const router = useRouter();
   const isMyDashboard = !router.pathname.includes('my');
+  const isEditPage = !router.pathname.includes('edit');
 
   const handleSideMenuToggle = () => {
     setIsOpenSideMenu((pre) => !pre);
@@ -38,7 +39,7 @@ function DashBoardHeader() {
                 width="24"
                 height="28"
                 src="/svgs/header-small-logo-purple.svg"
-                alt="헤더 메인 로고 이미지"
+                alt="헤더 로고 이미지"
               />
             ) : (
               <Image
@@ -54,22 +55,34 @@ function DashBoardHeader() {
         <div className={styles.box}>
           {isMyDashboard && (
             <>
+              {isEditPage && (
+                <Link className={styles.button} href="dashboard/edit">
+                  <Image
+                    className={styles.icon}
+                    src={SettngIcon}
+                    alt="설정 버튼 톱니바퀴 이미지"
+                  />
+                  {!isBreakPoint && '관리'}
+                </Link>
+              )}
               <button className={styles.button}>
                 <Image
                   className={styles.icon}
-                  src={SettngIcon}
-                  alt="톱니바퀴 이미지"
+                  src={InviteIcon}
+                  alt="초대하기 버튼 플러스 이미지"
                 />
-                {!isBreakPoint && '관리'}
-              </button>
-              <button className={styles.button}>
-                <Image className={styles.icon} src={InviteIcon} alt="invite" />
                 {!isBreakPoint && '초대하기'}
               </button>
             </>
           )}
           <div className={styles.box}>
-            <div className={styles.img}></div>
+            <Image
+              className={styles.img}
+              src=""
+              alt="유저 프로필 이미지"
+              width={38}
+              height={38}
+            />
             <div className={styles.name}>박유빈aaaaaaaaaaaaaa</div>
           </div>
         </div>
