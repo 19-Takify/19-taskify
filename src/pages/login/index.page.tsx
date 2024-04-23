9f83e4ef355478ec5853f53601e0b6048f56b350
Songeun <hannahkeeien@gmail.com>
2024-04-23 14:25:12 +0900

    feat: 로그인/회원가입 페이지 헤드 추가

--- src/pages/login/index.page.tsx
+++ src/pages/login/index.page.tsx
@@ -3,33 +3,39 @@ import Link from 'next/link';
 import styles from './Login.module.scss';
 import { PAGE_PATH } from '@/constants/pageUrl';
 import LoginForm from './components/LoginForm';
+import Head from 'next/head';
 
 function Login() {
   return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        <Link href={PAGE_PATH.MAIN}>
-          <Image
-            src="/svgs/taskify-vertical-logo.svg"
-            alt="Taskify 로고"
-            className={styles.logoImage}
-            width={200}
-            height={280}
-            priority
-          />
-        </Link>
-        <h2 className={styles.title}>오늘도 만나서 반가워요! </h2>
-      </header>
-      <LoginForm />
-      <footer>
-        <p className={styles.footerMessage}>
-          회원이 아니신가요?
-          <Link href={PAGE_PATH.SIGNUP} className={styles.link}>
-            회원가입하기
+    <>
+      <Head>
+        <title>Taskify | 로그인</title>
+      </Head>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <Link href={PAGE_PATH.MAIN}>
+            <Image
+              src="/svgs/taskify-vertical-logo.svg"
+              alt="Taskify 로고"
+              className={styles.logoImage}
+              width={200}
+              height={280}
+              priority
+            />
           </Link>
-        </p>
-      </footer>
-    </div>
+          <h2 className={styles.title}>오늘도 만나서 반가워요! </h2>
+        </header>
+        <LoginForm />
+        <footer>
+          <p className={styles.footerMessage}>
+            회원이 아니신가요?
+            <Link href={PAGE_PATH.SIGNUP} className={styles.link}>
+              회원가입하기
+            </Link>
+          </p>
+        </footer>
+      </div>
+    </>
   );
 }
 
