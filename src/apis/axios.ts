e69a71ee585c135e76430dcbbd09c9d3baa70bd0
woo29 <dldngur37@naver.com>
2024-04-23 11:04:30 +0900

    refactor: 임시 토큰 env로 관리

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const TEMP_TOKEN = process.env.NEXT_PUBLIC_TEMP_TOKEN;
 
 const instance = axios.create({
   baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Mjk5MiwidGVhbUlkIjoiNC0xOSIsImlhdCI6MTcxMzYwNDc4OCwiaXNzIjoic3AtdGFza2lmeSJ9.C_9YGNBlt7urk1ZNlVqL6KCuFlWozrrAd6rz0Q0YM8M`,
+    Authorization: `Bearer ${TEMP_TOKEN}`,
   },
 });
 export default instance;
