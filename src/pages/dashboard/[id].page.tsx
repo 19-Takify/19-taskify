19629a0fb4b33252632b4423ae69cd3eb5218723
woo29 <dldngur37@naver.com>
2024-04-23 20:47:44 +0900

    feat: 타이틀 추가

--- src/pages/dashboard/[id].page.tsx
+++ src/pages/dashboard/[id].page.tsx
@@ -1,12 +1,20 @@
 import DashBoardLayout from '@/components/Layout/DashBoardLayout';
 import React, { ReactElement } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 function Dashboard() {
   const router = useRouter();
   const { id } = router.query;
 
-  return <>{id}</>;
+  return (
+    <>
+      <Head>
+        <title>Taskify | 대시보드 이름 추가 예정</title>
+      </Head>
+      {id}
+    </>
+  );
 }
 
 Dashboard.getLayout = function getLayout(page: ReactElement) {
