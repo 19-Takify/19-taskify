40f0dd8eaf4ed68c3545fa482042f02ada72b204
woo29 <dldngur37@naver.com>
2024-04-24 12:37:25 +0900

    feat: 폴더 구조 추가, 대시보드 수정 페이지 레이아웃 적용

--- src/pages/dashboard/[id]/edit/index.page.tsx
+++ src/pages/dashboard/[id]/edit/index.page.tsx
@@ -3,9 +3,11 @@ import { useRouter } from 'next/router';
 import instance from '@/apis/axios';
 import setToast from '@/utils/setToast';
 import styles from './style/edit.page.module.scss';
-import DashboardManager from '@/pages/dashboard/edit/components/DashboardManager';
-import DashboardEdit from '@/pages/dashboard/edit/components/DashboardEdit';
+import DashboardManager from '@/pages/dashboard/[id]/edit/components/DashboardManager';
+import DashboardEdit from '@/pages/dashboard/[id]/edit/components/DashboardEdit';
 import Meta from '@/components/Meta';
+import DashBoardLayout from '@/components/Layout/DashBoardLayout';
+import { ReactElement } from 'react';
 
 // 대시보드 삭제 버튼 - 대시보드 생성자(전역 상태 관리)한테만 보이게 조건부 렌더링, 컨펌 모달
 function Edit() {
@@ -26,7 +28,6 @@ function Edit() {
   return (
     <>
       <Meta title="Taskify | 대시보드 수정" />
-      <div style={{ border: '1px solid #000', height: '70px' }}>임시 헤더</div>
       <div className={styles.wrap}>
         <div className={styles.box}>
           <div className={styles.buttonBox}>
@@ -59,5 +60,9 @@ function Edit() {
     </>
   );
 }
+
+Edit.getLayout = function getLayout(page: ReactElement) {
+  return <DashBoardLayout>{page}</DashBoardLayout>;
+};
 
 export default Edit;
