4d55e46a9d9def881f1ecb49f6d23f0ce123427a
Songeun <hannahkeeien@gmail.com>
2024-04-25 18:25:31 +0900

    feat: 계정 관리 페이지 예시 추가

---
+++ src/pages/bongsong/components/Box/index.tsx
@@ -0,0 +1,29 @@
+import { useAtom } from 'jotai';
+import { userAtom } from '@/store/auth';
+
+const nicknames = ['송은', '유빈', '승구', '우혁', '봉찬'];
+
+function Box() {
+  // 계정 관리 페이지 예시
+  const [user, setUser] = useAtom(userAtom);
+
+  return (
+    <>
+      <div>Box 컴포넌트</div>
+      <div>user_nickname: {user.nickname}</div>
+      <button
+        type="button"
+        onClick={() =>
+          setUser((pre) => ({
+            ...pre,
+            nickname: nicknames[Math.floor(Math.random() * nicknames.length)],
+          }))
+        }
+      >
+        내 정보 변경
+      </button>
+    </>
+  );
+}
+
+export default Box;
