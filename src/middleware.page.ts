0e1227d6ed389d0ad4d81dc47cf2c5cc533610b0
Songeun <hannahkeeien@gmail.com>
2024-04-25 16:42:52 +0900

    refactor: middleware 로그인 필수 페이지 경로 수정

--- src/middleware.page.ts
+++ src/middleware.page.ts
@@ -3,18 +3,19 @@ import type { NextRequest } from 'next/server';
 import { AUTH_TOKEN_COOKIE_NAME } from './constants/auth';
 import { getMeForMiddleware } from './utils/auth';
 
-const authUrlRegex = /^(\/)$|^(\/signup|\/login)/;
+const guestOnlyUrlRegex = /^(\/$|\/signup|\/login)/;
+const userOnlyUrlRegex = /^(\/mypage|\/dashboard|\/mydashboard)/;
 
 export async function middleware(request: NextRequest) {
   const pathName = request.nextUrl.pathname;
   const accessToken = request.cookies.get(AUTH_TOKEN_COOKIE_NAME)?.value;
   const isLogin = await getMeForMiddleware(accessToken);
 
-  if (isLogin && authUrlRegex.test(pathName)) {
+  if (isLogin && guestOnlyUrlRegex.test(pathName)) {
     return NextResponse.redirect(new URL('/mydashboard', request.url));
   }
 
-  if (!isLogin && request.nextUrl.pathname.startsWith('/dashboard')) {
+  if (!isLogin && userOnlyUrlRegex.test(pathName)) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 }
