eaa7b35e445a66c81371a0764de9bebda6d53a9f
woo29 <dldngur37@naver.com>
2024-04-25 14:54:45 +0900

    comment: 커스텀 훅 주석 추가

--- src/hooks/useCurrentUrl.ts
+++ src/hooks/useCurrentUrl.ts
@@ -1,5 +1,10 @@
 import { useRouter } from 'next/router';
 
+/*
+현재 페이지의 url을 가져오는 커스텀 훅(Meta 컴포넌트 url 동적 주입시 사용)
+ex) http://localhost:3000/dashboard/4/edit
+router.asPath = /dashboard/4/edit
+*/
 const useCurrentUrl = (): string => {
   const router = useRouter();
   return router.asPath;
