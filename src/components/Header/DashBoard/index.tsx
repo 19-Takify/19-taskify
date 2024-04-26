d41f2462c4366cff05c938eab1398fe6614482ec
woo29 <dldngur37@naver.com>
2024-04-26 21:45:15 +0900

    feat: 대시보드 헤더 대시보드 이름 추가

--- src/components/Header/DashBoard/index.tsx
+++ src/components/Header/DashBoard/index.tsx
@@ -10,6 +10,7 @@ import { useState } from 'react';
 import { logout } from '@/utils/auth';
 import { useAtomValue } from 'jotai';
 import { userAtom } from '@/store/auth';
+import { selectDashboardAtom } from '@/store/dashboard';
 
 function DashBoardHeader() {
   const [isOver, setIsover] = useState(false);
@@ -20,6 +21,7 @@ function DashBoardHeader() {
   const isMyDashboard = !router.pathname.includes('my');
   const isEditPage = !router.pathname.includes('edit');
   const user = useAtomValue(userAtom);
+  const selectDashboard = useAtomValue(selectDashboardAtom);
 
   const handleMouseOver = () => {
     setIsover(true);
@@ -66,7 +68,7 @@ function DashBoardHeader() {
             )}
           </Link>
           {isMyDashboard && (
-            <p className={styles.text}>내 대시보드11111111adsadsadsasa</p>
+            <p className={styles.text}>{selectDashboard.title}</p>
           )}
         </div>
         <div className={styles.box}>
