86fa13eacbf5c9448fb9682fc89eaaf57a7d4ac5
devwqc <devwqc@gmail.com>
2024-04-26 18:35:58 +0900

    comment: 필요없는 주석 제거

--- src/pages/dashboard/[id]/edit/index.page.tsx
+++ src/pages/dashboard/[id]/edit/index.page.tsx
@@ -12,10 +12,8 @@ import { GetServerSidePropsContext } from 'next';
 import { getMe } from '@/utils/auth';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  // axios interceptor에서 cookie를 가져오기 위한 필수 함수 호출
   setContext(context);
 
-  // user 전역 상태 초기값을 넣기 위한 user 객체
   const user = await getMe();
 
   return {
