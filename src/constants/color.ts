679d17812a4cb22f0cb4d795ecf034bc5e33e4e7
Songeun <hannahkeeien@gmail.com>
2024-04-28 03:22:03 +0900

    feat: color.ts 파일 추가

---
+++ src/constants/color.ts
@@ -0,0 +1,18 @@
+export const DASHBOARD_COLORS = [
+  '#7AC555',
+  '#760DDE',
+  '#FFA500',
+  '#76A5EA',
+  '#E876EA',
+];
+
+export const TAG_COLORS = [
+  '#E99695',
+  '#F9D0C4',
+  '#FEF2C0',
+  '#C2E0C6',
+  '#BFDADC',
+  '#C5DEF5',
+  '#BFD4F2',
+  '#D4C5F9',
+];
