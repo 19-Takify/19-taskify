a3517ede3e5360a73ad2c5e59e788c801bf2edfa
skoo1100 <skoo062125@gmail.com>
2024-04-29 12:34:54 +0900

    fix: 무한스크롤 테스트할 때 사용한 asdsadsadasda 제거

--- src/pages/dashboard/[id]/components/Column/index.tsx
+++ src/pages/dashboard/[id]/components/Column/index.tsx
@@ -434,7 +434,7 @@ function Column({
                             >
                               <Card cardData={cardData} />
                               {index === columnData.cards.length - 1 && (
-                                <div ref={sentinelRef}>asdsadsadasda</div>
+                                <div ref={sentinelRef}></div>
                               )}
                             </div>
                           )}
